cca602449b68feffd49171b93526dc54cf170c89
Bern <bernnystrom@gmail.com>
2021-01-31 00:57:49 +0800

    Re-format code with Prettier.

--- src/components/Burger/BuildControls/ControlOptions/ControlOptions.js
+++ src/components/Burger/BuildControls/ControlOptions/ControlOptions.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './ControlOptions.module.css';
 
 const controlOptions = ({ label }) => {
-    return (
-        <div className={styles.controlOptions}>
-            <div className={styles.label}>{label}</div>
-            <button className={styles.less}>Less</button>
-            <button className={styles.more}>More</button>
-        </div>
-    );
-}
+  return (
+    <div className={styles.controlOptions}>
+      <div className={styles.label}>{label}</div>
+      <button className={styles.less}>Less</button>
+      <button className={styles.more}>More</button>
+    </div>
+  );
+};
 
 export default controlOptions;

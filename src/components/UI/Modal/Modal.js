9c90775a9e88854b4760f216b641e02cc61ff58f
Bern <bernnystrom@gmail.com>
2021-01-31 07:40:13 +0800

    Add Backdrop component to modal.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
-const modal = ({ children, display }) => (
-  <div
-    className={styles.modal}
-    style={{
-      transform: display ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: display ? '1' : '0',
-    }}
-  >
-    {children}
-  </div>
+const modal = ({ children, isDisplay, isClosed }) => (
+  <>
+    <Backdrop displaySummary={isDisplay} closeSummary={isClosed} />
+    <div
+      className={styles.modal}
+      style={{
+        transform: isDisplay ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: isDisplay ? '1' : '0',
+      }}
+    >
+      {children}
+    </div>
+  </>
 );
 
 export default modal;

11806eadfeb7e166adaa07ddebbb253f573815f8
Bern <bernnystrom@gmail.com>
2021-01-31 01:42:55 +0800

    Set addItem prop from BuildControls component to an onClick listener to 'More' button.

--- src/components/Burger/BuildControls/ControlOptions/ControlOptions.js
+++ src/components/Burger/BuildControls/ControlOptions/ControlOptions.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './ControlOptions.module.css';
 
-const controlOptions = ({ label }) => {
+const controlOptions = ({ label, addItem }) => {
   return (
     <div className={styles.controlOptions}>
       <div className={styles.label}>{label}</div>
       <button className={styles.less}>Less</button>
-      <button className={styles.more}>More</button>
+      <button className={styles.more} onClick={addItem}>
+        More
+      </button>
     </div>
   );
 };

bbb263595b0acb0ad7abedc8f87c2db9e47f7f82
Bern <bernnystrom@gmail.com>
2021-01-31 00:43:08 +0800

    Refactor code formatting and add display message to user when burger ingredients are empty.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,16 +3,20 @@ import Ingredient from './Ingredient/Ingredient';
 import styles from './Burger.module.css';
 
 const burger = ({ ingredients }) => {
-  const transformedIngredients = Object.keys(ingredients).map((name) =>
-    [...Array(ingredients[name])].map((_, index) => {
-      return <Ingredient key={name + index} type={name} />;
-    })
-  );
+  let customIngredients = Object.keys(ingredients)
+    .map((name) =>
+      [...Array(ingredients[name])].map((_, index) => <Ingredient key={name + index} type={name} />)
+    )
+    .reduce((array, element) => [...array, ...element], []);
+
+  if (customIngredients.length === 0) {
+    customIngredients = <p>Got beef? Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={styles.burger}>
       <Ingredient type="bread-top" />
-      {transformedIngredients}
+      {customIngredients}
       <Ingredient type="bread-bottom" />
     </div>
   );

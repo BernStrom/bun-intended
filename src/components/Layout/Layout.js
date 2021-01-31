4b7c4c7d9a4b8489f46da49ee3bd843d93876d1c
Bern <bernnystrom@gmail.com>
2021-02-01 05:10:35 +0800

    Add Toolbar component into Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 
 const layout = (props) => {
   return (
     <>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={styles.content}>{props.children}</main>
     </>
   );

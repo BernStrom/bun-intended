7d15de48e6ec858ed26fd7ed79b179e5795db0ca
Bern <bernnystrom@gmail.com>
2021-02-02 08:17:07 +0800

    Change destructure props name to 'open' and 'close'.

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Backdrop.module.css';
 
-const backdrop = ({ displaySummary, closeSummary }) =>
-  displaySummary && <div className={styles.backdrop} onClick={closeSummary}></div>;
+const backdrop = ({ open, close }) =>
+  open ? <div className={styles.backdrop} onClick={close}></div> : null;
 
 export default backdrop;

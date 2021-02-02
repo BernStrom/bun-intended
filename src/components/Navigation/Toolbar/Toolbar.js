954fb043a70a5fa9e15092256430e0565e7f0f8e
Bern <bernnystrom@gmail.com>
2021-02-02 08:53:33 +0800

    Add toggle button component to nav toolbar with toggling prop.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import styles from './Toolbar.module.css';
 
-const toolbar = (props) => (
+const toolbar = ({ isToggle }) => (
   <header className={styles.toolbar}>
-    <div>MENU</div>
+    <DrawerToggle toggleClick={isToggle} />
     <div className={styles.logo}>
       <Logo />
     </div>

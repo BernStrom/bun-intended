285e85f219ed51f4671beda7ba3ab0cafeac00ae
Bern <bernnystrom@gmail.com>
2021-02-02 09:33:04 +0800

    Convert Modal to class component and add component lifecycle updates.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
-const modal = ({ children, isDisplay, isClosed }) => (
-  <>
-    <Backdrop open={isDisplay} close={isClosed} />
-    <div
-      className={styles.modal}
-      style={{
-        transform: isDisplay ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: isDisplay ? '1' : '0',
-      }}
-    >
-      {children}
-    </div>
-  </>
-);
+export default class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.isDisplay !== this.props.isDisplay;
+  }
 
-export default modal;
+  componentDidUpdate() {
+    console.log('[Modal] WillUpdate');
+  }
+
+  render() {
+    return (
+      <>
+        <Backdrop open={this.props.isDisplay} close={this.props.isClosed} />
+        <div
+          className={styles.modal}
+          style={{
+            transform: this.props.isDisplay ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.isDisplay ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}

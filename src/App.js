f49f50384a464ec63092ea0294d07fe135e59a8b
Bern <bernnystrom@gmail.com>
2021-02-04 16:44:19 +0800

    Add routing for homepage and order checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout">
+              <Checkout />
+            </Route>
+            <Route path="/" exact>
+              <BurgerBuilder />
+            </Route>
+          </Switch>
         </Layout>
       </div>
     );

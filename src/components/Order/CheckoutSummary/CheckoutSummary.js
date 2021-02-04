d783cb55616e628f782f3cc437541fc2c43cdb10
Bern <bernnystrom@gmail.com>
2021-02-04 16:42:45 +0800

    Update Button component with checkoutOption props

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -3,17 +3,17 @@ import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
 import styles from './CheckoutSummary.module.css';
 
-const checkoutSummary = ({ ingredients }) => {
+const checkoutSummary = ({ ingredients, cancelCheckout, continueCheckout }) => {
   return (
     <div className={styles.checkoutSummary}>
       <h1>We hope it tastes well!</h1>
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={ingredients} />
       </div>
-      <Button btnType="Danger" checkoutOption>
+          <Button btnType="Danger" checkoutOption={cancelCheckout}>
         CANCEL
       </Button>
-      <Button btnType="Success" checkoutOption>
+      <Button btnType="Success" checkoutOption={continueCheckout}>
         CONTINUE
       </Button>
     </div>

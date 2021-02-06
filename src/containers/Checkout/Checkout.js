0eda458dbd70d50ab0b153480ef340b58293b2eb
Bern <bernnystrom@gmail.com>
2021-02-06 15:22:08 +0800

    Update rendering of component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,7 +3,6 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-
 class Checkout extends Component {
   cancelCheckoutHandler = () => {
     this.props.history.goBack();
@@ -15,9 +14,13 @@ class Checkout extends Component {
 
   render() {
     let summary = <Redirect to="/" />;
+
     if (this.props.ings) {
-      summary = (
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
+
+      summary = ( 
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             cancelCheckout={this.cancelCheckoutHandler}
@@ -37,6 +40,7 @@ class Checkout extends Component {
 const mapStateToProps = (state) => {
   return {
     ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
 

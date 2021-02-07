f199ef2d7c90e862f8d992a5ba29808ad533eca9
Bern <bernnystrom@gmail.com>
2021-02-07 12:22:36 +0800

    Refactor components props

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import styles from './Order.module.css';
 
-const order = ({ price, ingredients }) => {
+const order = (props) => {
   const ingredients = [];
 
-  for (let ingredientName in ingredients) {
-    ingredients.push({ name: ingredientName, amount: ingredients[ingredientName] });
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
   }
 
-  const ingredientOutput = ingredients.map((ig) => (
-    <span
-      key={ig.name}
-      style={{
-        textTransform: 'capitalize',
-        display: 'inline-block',
-        margin: '0 8px',
-        padding: '5px',
-        border: '1px solid #ccc',
-      }}
-    >
-      {ig.name} ({ig.amount})
-    </span>
-  ));
+  const ingredientOutput = ingredients.map((ig) => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px',
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
 
   return (
     <div className={styles.order}>
       <p>Ingredients: {ingredientOutput}</p>
       <p>
-        Price: <strong>${price.toFixed(2)}</strong>
+        Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong>
       </p>
     </div>
   );

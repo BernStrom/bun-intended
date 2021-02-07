b5e9c5383e3ae5ddc609719406963f747492f3aa
Bern <bernnystrom@gmail.com>
2021-02-07 12:07:39 +0800

    Refactor and add conditional redering of NavItem components based on authentication

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,10 +2,17 @@ import React from 'react';
 import NavItem from './NavItem/NavItem';
 import styles from './NavigationItems.module.css';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul className={styles.navigationItems}>
-    <NavItem link="/" exact>Burger Builder</NavItem>
-    <NavItem link="/orders">Orders</NavItem>
+    <NavItem link="/" exact>
+      Burger Builder
+    </NavItem>
+    {props.isAuthenticated ? <NavItem link="/orders">Orders</NavItem> : null}
+    {!props.isAuthenticated ? (
+      <NavItem link="/auth">Authenticate</NavItem>
+    ) : (
+      <NavItem link="/logout">Logout</NavItem>
+    )}
   </ul>
 );
 

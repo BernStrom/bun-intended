63d7a2750491ef8f9f90592df007903f92be4426
Bern <bernnystrom@gmail.com>
2021-02-07 12:27:14 +0800

    Refactor component props

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Backdrop.module.css';
 
-const backdrop = ({ open, close }) =>
-  open ? <div className={styles.backdrop} onClick={close}></div> : null;
+const backdrop = (props) =>
+  props.show ? <div className={styles.backdrop} onClick={props.clicked}></div> : null;
 
 export default backdrop;

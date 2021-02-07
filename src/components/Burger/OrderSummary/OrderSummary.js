586316158fe90cb598c2b6c957018015d778fb52
Bern <bernnystrom@gmail.com>
2021-02-07 11:57:20 +0800

    Refactor to class component to maintain consistency of using class-based

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ({ ingredients, price, cancelCheckout, proceedCheckout }) => {
-  const ingredientsSummary = Object.keys(ingredients).map((name) => (
-    <li key={name}>
-      <span style={{ textTransform: 'capitalize' }}>{name}</span>: {ingredients[name]}
-    </li>
-  ));
+class OrderSummary extends Component {
+  // Will refactor this back to a functional component using hooks later.
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
 
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>
-        <em>With these oh-so-satisfying ingredients:</em>
-      </p>
-      <ul>{ingredientsSummary}</ul>
-      <p>
-        <strong>Total Price: {price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="danger" checkoutOption={cancelCheckout}>
-        CANCEL
-      </Button>
-      <Button btnType="success" checkoutOption={proceedCheckout}>
-        CONTINUE
-      </Button>
-    </>
-  );
-};
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;

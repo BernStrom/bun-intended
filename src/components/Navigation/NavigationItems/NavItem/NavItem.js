ee9c9216457afc7fd7a02ea9a685d315c76d17be
Bern <bernnystrom@gmail.com>
2021-02-07 12:09:51 +0800

    Refactor component props

--- src/components/Navigation/NavigationItems/NavItem/NavItem.js
+++ src/components/Navigation/NavigationItems/NavItem/NavItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './NavItem.module.css';
 
-const navItem = ({ children, link, exact }) => (
-  <li className={styles.navItem}>
-    <NavLink to={link} exact={exact} activeClassName={styles.active}>
-      {children}
+const navItem = (props) => (
+  <li className={styles.navigationItem}>
+    <NavLink to={props.link} exact={props.exact} activeClassName={styles.active}>
+      {props.children}
     </NavLink>
   </li>
 );

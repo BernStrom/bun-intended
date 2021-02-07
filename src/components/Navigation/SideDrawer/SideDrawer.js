e3a78a832320916579b6004d0fe9f2ec92f46e15
Bern <bernnystrom@gmail.com>
2021-02-07 12:13:14 +0800

    Refactor component and add authenticated navigation items component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,19 +4,22 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import styles from './SideDrawer.module.css';
 
-const sideDrawer = ({ isOpen, isClosed }) => {
-  let attachedStyles = [styles.sideDrawer, styles.close];
-  if (isOpen) attachedStyles = [styles.sideDrawer, styles.open];
+const sideDrawer = (props) => {
+  let attachedClasses = [styles.sideDrawer, styles.close];
+
+  if (props.open) {
+    attachedClasses = [styles.sideDrawer, styles.open];
+  }
 
   return (
     <>
-      <Backdrop open={isOpen} close={isClosed} />
-      <div className={attachedStyles.join(' ')}>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
         <div className={styles.logo}>
           <Logo />
         </div>
         <nav>
-          <NavigationItems />
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </>

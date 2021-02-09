fa17d928dadea5d58ca72dc7777dfb042fa6f8d5
Bern <bernnystrom@gmail.com>
2021-02-09 17:24:06 +0800

    Convert to func component using React.memo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-  }
+const modal = (props) => {
+  return (
+    <>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={styles.modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
-          className={styles.modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0',
-          }}
-        >
-          {this.props.children}
-        </div>
-      </>
-    );
-  }
-}
-
-export default Modal;
+export default React.memo(
+  modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show && nextProps.children === prevProps.children
+);
